917063d75f309a1e825520807883d046763b1721
DannyWong <danny.96.wong@gmail.com>
2022-05-10 20:32:18 -0700

    feat: more work on mayor choose modal

--- components/MayorPickModal.jsx
+++ components/MayorPickModal.jsx
@@ -1,16 +1,28 @@
 import {
-  Modal,
-  ModalOverlay,
-  ModalContent,
-  ModalHeader,
-  ModalFooter,
-  ModalBody,
-  ModalCloseButton,
+  useDisclosure, Button, Modal, ModalContent, ModalOverlay,
+  ModalHeader, ModalBody, ModalCloseButton,
 } from '@chakra-ui/react';
 
-function MayorPickModal({ onMayorPick }) {
+function MayorPickModal({ lobby, onMayorPick }) {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const onWordPick = (e) => {
+    // close modal
+    onMayorPick(e.target.value);
+  };
+
   return (
-    <div> Modal</div>
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>Mayor! Choose a word!</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>
+          <Button onClick={(e) => onWordPick(e)}>{lobby.words[0]}</Button>
+          <Button onClick={(e) => onWordPick(e)}>{lobby.words[1]}</Button>
+        </ModalBody>
+      </ModalContent>
+    </Modal>
   );
 }
 
