60a9cadd9c24d6cc959020662aa65ba07c227a0c
bogdangordin <bogdan.gordin@gmail.com>
2022-05-11 14:11:12 -0700

    completed the voting components

--- components/WerewolfVote.jsx
+++ components/WerewolfVote.jsx
@@ -9,37 +9,46 @@ modal for the werewolf and any other special role to vote on the seer if the tim
 the choice for vote is done with the table select
 */
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import styled from 'styled-components';
 
 import { socket } from '../pages/api/service/socket';
 
 
 
-function WerewolfVote({ lobby }) {
+function WerewolfVote({ lobby, loginData }) {
 
-  const [currVote, setCurrVote] = useState(undefined);
-
+  const [currVote, setCurrVote] = useState('---');
 
   const clickedOnButton = (e) => {
-    socket.emit('VoteSeer', { name: currVote });
+    if (currVote === '---') {
+      return null;
+    }
+    socket.emit('VoteSeer', { player: lobby.players[currVote], lobbyName: lobby?.name });
+    console.log(e.target.id);
+    document.getElementById(e.target.id).style.display = 'none'; // might not re-appear with new game???
   };
 
+  const pickedDrop = (e) => {
+    setCurrVote(e.target.value);
+  };
+
+  console.log(lobby);
 
   return (
     <Container id="WerewolfVote">
       <WhoIsP>WHO IS THE SEER?</WhoIsP>
 
       <div>
-        <ChooseS id="PlayersDrop" name="players">
-          <option value="DEFAULT" disabled>---</option>
-          {/* {players.map((p) =>
-            <option value={p.username}>{p.username}</option>)} */}
+        <ChooseS id="PlayersDrop" name="players" onChange={(e) => { pickedDrop(e); }}>
+          <option value="DEFAULT" selected disabled>---</option>
+          {Object.keys(lobby.players).map((p) =>
+            loginData.name !== p && <option value={p}>{p}</option>)}
         </ChooseS>
       </div>
 
       <div>
-        <button id="Submit" type="button" onClick={(e) => { clickedOnButton(e); }}>SUBMIT</button>
+        <button id="SubmitWeVote" type="button" onClick={(e) => { clickedOnButton(e); }}>SUBMIT</button>
       </div>
 
     </Container>
