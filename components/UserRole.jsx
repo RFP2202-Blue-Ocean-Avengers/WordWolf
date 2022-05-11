6d97eec226c9fc4f9f678636b4da58a088b9a19c
Casey Eads <eadscaseyl@gmail.com>
2022-05-10 18:58:01 -0700

    chore: save update to role modal.

--- components/UserRole.jsx
+++ components/UserRole.jsx
@@ -1,3 +1,19 @@
-function UserRole() {
-  return 'something';
+import {
+  RoleContainer,
+  InnerRoleContainer,
+  Img,
+  Banner,
+} from './ModalStyles/UserRoleCard';
+
+function UserRole({ player }) {
+  return (
+    <RoleContainer>
+      <InnerRoleContainer>
+        <Img src="" />
+        <Banner>{player.role}</Banner>
+      </InnerRoleContainer>
+    </RoleContainer>
+  );
 }
+
+export default UserRole;
