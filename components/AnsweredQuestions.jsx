b711663441379685a2ad139af03b592c6358f6f8
DannyWong <danny.96.wong@gmail.com>
2022-05-11 22:17:55 -0700

    feat: start work on Answered Questions Display

---
+++ components/AnsweredQuestions.jsx
@@ -0,0 +1,13 @@
+import { Box, List, ListIcon, ListItem } from '@chakra-ui/react';
+
+function AnsweredQuestions({ lobby }) {
+  return (
+    <Box>
+      <List>
+
+      </List>
+    </Box>
+  );
+}
+
+export default AnsweredQuestions;
