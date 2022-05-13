6c3542ccef58dcd99a7f0e94105c8316266535ec
Eunice Kim <euniceyunjkim@gmail.com>
2022-05-12 19:18:34 -0700

    fix/feat: update modals functionality for all players

--- components/MayorPickModal.jsx
+++ components/MayorPickModal.jsx
@@ -1,30 +1,31 @@
 import {
-  useDisclosure, Button, Modal, ModalContent, ModalOverlay,
-  ModalHeader, ModalBody, ModalFooter,
+  Button, Modal, ModalContent, ModalOverlay,
+  ModalHeader, ModalBody, ModalFooter, Box,
 } from '@chakra-ui/react';
 
 import { useEffect } from 'react';
 
-function MayorPickModal({ lobby, onMayorPick }) {
-  const { isOpen, onOpen, onClose } = useDisclosure();
-
+function MayorPickModal({ lobby, onMayorPick, loginData }) {
   const onWordPick = (e) => {
-    onClose();
     onMayorPick(e.target.name);
   };
 
-  useEffect(() => {
-    onOpen();
-  }, []);
-
   return (
-    <Modal isOpen={isOpen}>
+    <Modal isOpen={lobby.gameState === 'mayorPick'}>
       <ModalOverlay />
       <ModalContent display="flex" flexDirection="column">
-        <ModalHeader textAlign="center">Mayor! Choose a word!</ModalHeader>
-        <ModalBody display="flex" justifyContent="space-evenly">
-          <Button background="#D19E61" name={lobby.words[0]} onClick={(e) => onWordPick(e)}>{lobby.words[0]}</Button>
-          <Button background="#D19E61" name={lobby.words[1]} onClick={(e) => onWordPick(e)}>{lobby.words[1]}</Button>
+        <ModalHeader textAlign="center">
+          {lobby?.mayor?.name === loginData.name
+            ? <Box>Mayor! Choose a word!</Box> : <Box>Mayor is choosing a word!</Box>}
+        </ModalHeader>
+        <ModalBody>
+          {lobby?.mayor?.name === loginData.name
+            ? (
+              <Box display="flex" justifyContent="space-evenly">
+                <Button background="#D19E61" name={lobby.words[0]} onClick={(e) => onWordPick(e)}>{lobby.words[0]}</Button>
+                <Button background="#D19E61" name={lobby.words[1]} onClick={(e) => onWordPick(e)}>{lobby.words[1]}</Button>
+              </Box>
+            ) : <Box display="flex" justifyContent="center">Please wait!</Box>}
         </ModalBody>
         <ModalFooter />
       </ModalContent>
--- components/WerewolfVote.jsx
+++ components/WerewolfVote.jsx
@@ -8,7 +8,14 @@ Blue Ocean
 modal for the werewolf and any other special role to vote on the seer if the time has ran out
 the choice for vote is done with the table select
 */
-
+import {
+  Modal,
+  ModalContent,
+  ModalOverlay,
+  ModalHeader,
+  ModalBody,
+  Box,
+} from '@chakra-ui/react';
 import { useState, useContext } from 'react';
 import styled from 'styled-components';
 
@@ -32,19 +39,31 @@ function WerewolfVote({ lobby, loginData }) {
   };
 
   return (
-    <Container id="WerewolfVote">
-      <WhoIsP>WHO IS THE SEER?</WhoIsP>
-      <div>
-        <ChooseS id="PlayersDrop" name="players" onChange={(e) => { pickedDrop(e); }}>
-          <option value="DEFAULT" selected disabled>---</option>
-          {lobby && Object.keys(lobby?.players)
-            .map((p) => loginData.name !== p && <option value={p}>{p}</option>)}
-        </ChooseS>
-      </div>
-      <div>
-        {voted ? null : <button id="SubmitWeVote" type="button" onClick={(e) => { clickedOnButton(e); }}>SUBMIT</button>}
-      </div>
-    </Container>
+    <Modal isOpen={lobby.gameState === 'wordGuessed'}>
+      <ModalOverlay />
+      <ModalContent display="flex" justifyContent="center" alignItems="center" textAlign="center">
+        <ModalHeader>
+          {lobby?.players[loginData.name].role !== 'werewolf'
+            ? <h1>VOTING ROUND</h1> : <h1>WHO IS THE SEER?</h1>}
+        </ModalHeader>
+        <ModalBody>
+          {lobby?.players[loginData.name].role !== 'werewolf'
+            ? (
+              <h3>The wolves are currently voting...</h3>
+            )
+            : (
+              <Box display="flex" flexDirection="column">
+                <ChooseS id="PlayersDrop" name="players" onChange={(e) => { pickedDrop(e); }}>
+                  <option value="DEFAULT" selected disabled>---</option>
+                  {lobby && Object.keys(lobby?.players)
+                    .map((p) => loginData.name !== p && <option value={p}>{p}</option>)}
+                </ChooseS>
+                {voted ? null : <Box as="button" backgroundColor="#C4C4C4" marginTop="10" id="SubmitWeVote" onClick={(e) => { clickedOnButton(e); }}>SUBMIT</Box>}
+              </Box>
+            )}
+        </ModalBody>
+      </ModalContent>
+    </Modal>
   );
 }
 
@@ -53,21 +72,5 @@ export default WerewolfVote;
 
 // document.getElementById(e.target.id).style.borderBottom = '8px solid LightSkyBlue';
 
-const Container = styled.section`
-width: 180px;
-height: 105px;
-text-align: center;
-border-radius: 30px;
-background-color: #F8F8F8;
-`;
-
-const WhoIsP = styled.p`
-padding-top: 15px;
-margin: 0px auto;
-width: fit-content;
-color: black;
-`;
-
 const ChooseS = styled.select`
-background-color: #E0E0E0;
 `;
